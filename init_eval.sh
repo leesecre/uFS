408c4c38017809c3131f6c8172da2373327fbbff
Junho Ahn <ahn9807@gmail.com>
2024-04-10 05:26:40 +0000

    Done filebench

--- init_eval.sh
+++ init_eval.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-cp ./record_cpu_util.py ~/workspace/uFS-bench/filebench/
-cp ./record_cpu_util.py ~/workspace/uFS-bench/FS_microbench/
+## cpu utils
+cp ./ext_scripts/record_cpu_util.py ~/workspace/uFS-bench/filebench/
+cp ./ext_scripts/record_cpu_util.py ~/workspace/uFS-bench/FS_microbench/
 
-cp ./run_varmail_ext4.py ~/workspace/uFS-bench/filebench/scripts/
-cp ./run_varmail_ufs.py ~/workspace/uFS-bench/filebench/scripts/
-cp ./run_webserver_ufs.py ~/workspace/uFS-bench/filebench/scripts/
-cp ./run_webserver_ext4.py ~/workspace/uFS-bench/filebench/scripts/
+## filebench
+cp ./ext_scripts/run_varmail_ufs.py ~/workspace/uFS-bench/filebench/scripts/
+cp ./ext_scripts/run_varmail_ext4.py ~/workspace/uFS-bench/filebench/scripts/
+cp ./ext_scripts/run_webserver_ufs.py ~/workspace/uFS-bench/filebench/scripts/
+cp ./ext_scripts/run_webserver_ext4.py ~/workspace/uFS-bench/filebench/scripts/
+cp ./ext_scripts/run_fileserver_ufs.py ~/workspace/uFS-bench/filebench/scripts/
+cp ./ext_scripts/run_fileserver_ext4.py ~/workspace/uFS-bench/filebench/scripts/
+
+## FS_micro
 cp ./cfs_bench/exprs/artifact_eval/run-fs_micro.sh ~/workspace/uFS/cfs_bench/exprs/artifact_eval/run-fs_micro.sh
-cp ./run_fs_micro_all_ext4.py ~/workspace/uFS-bench/FS_microbench/scripts/
-cp ./run_fs_micro_all_ufs.py ~/workspace/uFS-bench/FS_microbench/scripts/
 cp -r ./FS_microbench ~/workspace/uFS-bench/
+cp ./ext_scripts/run_fs_micro_all_ext4.py ~/workspace/uFS-bench/FS_microbench/scripts/
+cp ./ext_scripts/run_fs_micro_all_ufs.py ~/workspace/uFS-bench/FS_microbench/scripts/
